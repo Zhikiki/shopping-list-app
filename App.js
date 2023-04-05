35d2193d802f39eb20fcba4ec458f37323e20d07
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-04-05 22:16:10 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import firebase functions for inicialisation Cloud Firestore
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// import the screens
+import ShoppingLists from './components/ShoppingLists';
+import ShoppingListsRealTime from './components/ShoppingListsRealTime';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBpN2c6Pdh1yGcjb8JASZzDNW2nVImo6Y0',
+    authDomain: 'shopping-list-demo-9d936.firebaseapp.com',
+    projectId: 'shopping-list-demo-9d936',
+    storageBucket: 'shopping-list-demo-9d936.appspot.com',
+    messagingSenderId: '797816656388',
+    appId: '1:797816656388:web:de133c86a74ea731f1db51',
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='ShoppingLists'>
+        <Stack.Screen name='ShoppingLists'>
+          {/* {(props) => <ShoppingLists db={db} {...props} />} */}
+          {(props) => <ShoppingListsRealTime db={db} {...props} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ components/ShoppingListsRealTime.js
@@ -0,0 +1,173 @@
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Alert,
+} from 'react-native';
+
+// import firebase functions for quering data
+import { collection, getDocs, addDoc, onSnapshot } from 'firebase/firestore';
+
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const ShoppingListsRealTime = ({ db }) => {
+  const [lists, setLists] = useState([]);
+  const [listName, setListName] = useState('');
+  const [item1, setItem1] = useState('');
+  const [item2, setItem2] = useState('');
+
+  /* useEffect() attaches listener only once, when component is mounted
+  [] - dependency array is empty, we don't need to call useEffect more then once
+  it will will be automatically run whenever there’s a change in the targeted database reference
+  onSnapshot() - cheks whether there were any changes in collection and its documents. Arguments:
+  - collection(db, 'shoppinglists') - reference that you attach the listener to
+  - The callback function that’s called whenever there’s a change detected in the reference.
+  In this case in callback function we get id and key/value of the items and pushh them to newList array
+  then we set newLists as a value for lists setLists(newLists);
+  */
+  useEffect(() => {
+    const unsubShoppinglists = onSnapshot(
+      collection(db, 'shoppinglists'),
+      (documentsSnapshot) => {
+        let newLists = [];
+        documentsSnapshot.forEach((doc) => {
+          newLists.push({ id: doc.id, ...doc.data() });
+        });
+        setLists(newLists);
+      }
+    );
+    // code to execute when the component will be unmounted
+    // to clean the memory when listener is not needed any more
+    return () => {
+        if (unsubShoppinglists) unsubShoppinglists()
+    }
+  }, []);
+
+  const addShoppingList = async (newList) => {
+    /*addDoc will add new document to collection and generate id
+    we use db - database intialised in the App.js
+    shoppinglists - name of the collection
+    newList - object created from user inputs (listName, item1, item2)*/
+    const newListRef = await addDoc(collection(db, 'shoppinglists'), newList);
+
+    /*If new document was successfuly created .id and .data() will be set on it.
+    if newListRef.id exists (true) - alert success message
+    if newListRef.id doesn't exist (false) - alert fail message*/
+    if (newListRef.id) {
+      setLists([newList, ...lists]);
+      Alert.alert(`The list '${listName}' has been added`);
+    } else {
+      Alert.alert(`Unable to add. Please try later`);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.listsContainer}
+        data={lists}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>
+              {/* The array function join() converts an array into a string.
+             adds a separator string between each element from the array once concatenated*/}
+              {item.name}: {item.items.join(', ')}
+            </Text>
+          </View>
+        )}
+      />
+      <View style={styles.listForm}>
+        <TextInput
+          style={styles.listName}
+          placeholder='List Name'
+          value={listName}
+          onChangeText={setListName}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder='Item #1'
+          value={item1}
+          onChangeText={setItem1}
+        />
+        <TextInput
+          style={styles.item}
+          placeholder='Item #2'
+          value={item2}
+          onChangeText={setItem2}
+        />
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => {
+            const newList = { name: listName, items: [item1, item2] };
+            addShoppingList(newList);
+          }}
+        >
+          <Text style={styles.addButtonText}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      {Platform.OS === 'ios' ? (
+        <KeyboardAvoidingView behavior='padding' />
+      ) : null}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listItem: {
+    height: 70,
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    borderBottomWidth: 1,
+    borderBottomColor: '#AAA',
+    flex: 1,
+    flexGrow: 1,
+  },
+  listForm: {
+    flexBasis: 275,
+    flex: 0,
+    margin: 15,
+    padding: 15,
+    backgroundColor: '#CCC',
+  },
+  listName: {
+    height: 50,
+    padding: 15,
+    fontWeight: '600',
+    marginRight: 50,
+    marginBottom: 15,
+    borderColor: '#555',
+    borderWidth: 2,
+  },
+  item: {
+    height: 50,
+    padding: 15,
+    marginLeft: 50,
+    marginBottom: 15,
+    borderColor: '#555',
+    borderWidth: 2,
+  },
+  addButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: '#000',
+    color: '#FFF',
+  },
+  addButtonText: {
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+});
+
+export default ShoppingListsRealTime;
